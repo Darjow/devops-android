abbc617c7f740fdcc26298c980fcea51fdc0367a
darjow <dariobronders123@hotmail.com>
2023-01-03 13:47:29 +0100

    add global object for configs

--- android/app/src/main/java/com/hogent/android/network/Config.kt
+++ android/app/src/main/java/com/hogent/android/network/Config.kt
@@ -1,7 +1,25 @@
 package com.hogent.android.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 class Config {
     companion object{
         const val BASE_URL = "http://localhost:9000/api/"
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        fun createRetrofit(endpoint : String): Retrofit{
+            return Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .baseUrl(endpoint)
+                .build()
+        }
+
+
+
     }
 }
--- android/app/src/main/java/com/hogent/android/network/ContractApiService.kt
+++ android/app/src/main/java/com/hogent/android/network/ContractApiService.kt
@@ -1,5 +1,13 @@
 package com.hogent.android.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 private const val API = Config.BASE_URL + "contract"
+
+private val moshi = Config.moshi
+private val retrofit = Config.createRetrofit(API)
 
 
