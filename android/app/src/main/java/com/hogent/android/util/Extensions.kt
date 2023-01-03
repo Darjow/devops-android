7c44b3a10f4accf909daa58396b477ecd43860c5
darjow <dariobronders123@hotmail.com>
2023-01-03 13:12:27 +0100

    add extension method in mock

--- android/app/src/main/java/com/hogent/android/util/Extensions.kt
+++ android/app/src/main/java/com/hogent/android/util/Extensions.kt
@@ -3,10 +3,13 @@ package com.hogent.android.util
 import android.content.Context
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import java.time.LocalDate
 
 
-    fun View.closeKeyboardOnTouch() {
+fun View.closeKeyboardOnTouch() {
         setOnTouchListener { v, event ->
             if(event.actionMasked and event.action != MotionEvent.ACTION_MOVE and  MotionEvent.ACTION_SCROLL) {
                     val inputMethodManager =
@@ -17,3 +20,16 @@ import android.view.inputmethod.InputMethodManager
             false
         }
     }
+
+ fun ViewGroup.clearForm(){
+    for (i in 0 until this.childCount){
+        when(val child = this.getChildAt(i)){
+            is Spinner -> child.setSelection(0)//geen waarde voor spinner
+            is EditText -> child.setText("")
+            is SeekBar -> child.progress = 0
+            is DatePicker -> child.updateDate(LocalDate.now().year, LocalDate.now().monthValue, LocalDate.now().dayOfMonth)
+            is RadioGroup -> child.clearCheck()
+            is ViewGroup -> child.clearForm() //recursief om de edit en spinners te clearen
+        }
+    }
+}
