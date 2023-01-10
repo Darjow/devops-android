5468b2d3dffef9ba41f0894558e5d3372f9c05ab
Arthur Libberecht <arthurlibberecht@student.hogent.be>
2023-01-10 18:49:25 +0100

    register werkt

--- backend/src/data/migrations/202212281410_createCustomerTable.js
+++ backend/src/data/migrations/202212281410_createCustomerTable.js
@@ -3,23 +3,23 @@ const {tables} = require('..')
 module.exports = {
     up: async (knex) => {
         await knex.schema.createTable(tables.customer, (table) => {
-            table.integer('id').primary();
+            table.increments("id").primary();
             table.string('firstname',40).notNullable();
             table.string('lastname',40).notNullable();
             table.string('email', 40).notNullable();
             table.string('password', 40).notNullable();
             table.string('phonenumber',40).notNullable();
-            table.string('bedrijf_opleiding',40).nullable();
+            table.string('bedrijf_opleiding',40).nullable().defaultTo("");
 
-            table.string("contact1_phone", 20).nullable()
-            table.string("contact1_email", 40).nullable()
-            table.string("contact1_firstname", 20).nullable()
-            table.string("contact1_lastname", 20).nullable()
+            table.string("contact1_phone", 20).nullable().defaultTo("");
+            table.string("contact1_email", 40).nullable().defaultTo("");
+            table.string("contact1_firstname", 20).nullable().defaultTo("");
+            table.string("contact1_lastname", 20).nullable().defaultTo("");
            
-            table.string("contact2_phone", 20).nullable()
-            table.string("contact2_email", 40).nullable()
-            table.string("contact2_firstname", 20).nullable()
-            table.string("contact2_lastname", 20).nullable()
+            table.string("contact2_phone", 20).nullable().defaultTo("");
+            table.string("contact2_email", 40).nullable().defaultTo("");
+            table.string("contact2_firstname", 20).nullable().defaultTo("");
+            table.string("contact2_lastname", 20).nullable().defaultTo("");
 
         })
     },
